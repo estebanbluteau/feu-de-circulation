424c77f30712585fd0f4d548dd3744d5a4f725b0
estebanbluteau <116097642+estebanbluteau@users.noreply.github.com>
2022-11-16 14:40:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
-pins.digitalWritePin(DigitalPin.P8, 1)
+input.onButtonPressed(Button.A, function () {
+    etat_pieton = 1
+})
+let etat_pieton = 0
+pins.digitalWritePin(DigitalPin.P16, 1)
 basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(8000)
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -7,17 +12,21 @@ basic.forever(function () {
     basic.pause(2500)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(8000)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P8, 0)
+    if (etat_pieton == 1) {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        etat_pieton = 0
         basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(250)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(250)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     }
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    basic.pause(6000)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(8000)
 })
