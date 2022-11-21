3a55fa3876795aad5a17ff319db16b025a23ab7f
estebanbluteau <116097642+estebanbluteau@users.noreply.github.com>
2022-11-21 16:05:42 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ input.onButtonPressed(Button.A, function () {
     etat_pieton = 1
 })
 let etat_pieton = 0
+etat_pieton = 0
 pins.digitalWritePin(DigitalPin.P16, 1)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(8000)
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -12,11 +12,10 @@ basic.forever(function () {
     basic.pause(2500)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    if (etat_pieton == 1) {
-        pins.digitalWritePin(DigitalPin.P16, 0)
+    if (etat_pieton == 1 && pins.digitalReadPin(DigitalPin.P2) == 1) {
         pins.digitalWritePin(DigitalPin.P8, 1)
-        etat_pieton = 0
-        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        basic.pause(500)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P16, 1)
         basic.pause(500)
@@ -27,6 +26,17 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P16, 0)
         basic.pause(250)
         pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(2000)
+        etat_pieton = 0
+    } else {
+        basic.pause(4000)
     }
-    basic.pause(8000)
+    basic.pause(4000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    etat_pieton = 0
+})
+basic.forever(function () {
+    if (etat_pieton > 1) {
+        etat_pieton += -1
+    }
 })
